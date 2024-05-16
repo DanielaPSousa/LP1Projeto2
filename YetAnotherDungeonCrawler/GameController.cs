2e42422bfda0af93a998109f102ef2c80d842186
DanielaPeixotoWork <danielapeixoto.work@gmail.com>
2024-05-16 21:21:06 +0100

    Verifica se tem itens na sala onde o jogador está, como o jogador apanha o item e mostra que foi apanhado, logo em seguide remove-o, e repete-se mas com o Sparklychest

--- YetAnotherDungeonCrawler/GameController.cs
+++ YetAnotherDungeonCrawler/GameController.cs
@@ -25,4 +25,30 @@ public class GameController
         }
         consoleView.DisplayPlayerInfo(player);
     }
+
+    public void PickUpItem()
+{
+
+    if (player.CurrentRoom.Item != null)
+    {
+        player.PickUpItem(player.CurrentRoom.Item);
+        
+        consoleView.DisplayMessage("Você apanhou o item.");
+        
+        player.CurrentRoom.Item = null;
+    }
+    else if (player.CurrentRoom.Treasure != null)
+    {
+        player.PickUpItem(player.CurrentRoom.Treasure);
+        
+        consoleView.DisplayMessage("Parabéns! Você conseguiu apanhar o Super Sparkly Chest!");
+        
+        player.CurrentRoom.Treasure = null;
+    }
+    else
+    {
+        consoleView.DisplayMessage("Não há itens nesta sala.");
+    }
+
+    consoleView.DisplayPlayerInfo(player);
 }
