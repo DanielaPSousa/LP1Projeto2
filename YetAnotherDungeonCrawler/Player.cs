003deea2cf18fe622461230f38f63abab826d97c
DanielaPeixotoWork <danielapeixoto.work@gmail.com>
2024-05-16 20:55:44 +0100

    Construtor da classe para inicializar a saude do jogador, ataque e começar com 0 moedas

--- YetAnotherDungeonCrawler/Player.cs
+++ YetAnotherDungeonCrawler/Player.cs
@@ -6,4 +6,11 @@ public class Player
     public int AttackPower { get; private set; }
     public Room CurrentRoom { get; set; }
     public int Coins { get; private set; }
+
+    public Player(int health, int attackPower)
+    {
+        Health = health;           
+        AttackPower = attackPower; 
+        Coins = 0;                 
+    }
 }
