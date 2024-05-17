d8b043be37f14046c29f8b4efc77891a194e5421
Daniela Sousa <danielasousa.info@gmail.com>
2024-05-17 15:48:52 +0100

    Criado propriedades para os respetivos métodos

---
+++ YetAnotherDungeonCrawler/Room.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+public class Room
+{
+    public string Description { get; set; }
+    public Dictionary<string, Room> Exits { get; private set; }
+    public Enemy Enemy { get; set; }
+    public Item Item { get; set; }
+    public SparklyChest Treasure { get; set; }   
+}
