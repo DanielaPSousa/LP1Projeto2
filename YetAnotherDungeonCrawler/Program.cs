900601ab64307d8c7a823745a5390a74b7bfa6a7
AlexandreAlmeid <alexandreruizdealmeida@gmail.com>
2024-05-25 17:19:06 +0100

    ProgramXML

--- YetAnotherDungeonCrawler/Program.cs
+++ YetAnotherDungeonCrawler/Program.cs
@@ -1,10 +1,17 @@
 using System;
-
+/// <summary>
+/// Class that implements the main function, from where the program starts running.
+/// </summary>
 public class Program
 {
+    /// <summary>
+    /// Main function that sets up the game environment, according to the MVC design pattern.
+    /// </summary>
+    /// <param name="args">The program takes no arguments, but could be used as initial setup inputs (e.g. name of the file containing the room map) in the future.</param>
     public static void Main(string[] args)
     {
         Controller gameController = new Controller();
-        gameController.StartGame();
+        IView view = new IView();
+        gameController.StartGame(view);
     }
 }
